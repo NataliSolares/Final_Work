8f39432846c1fb05a30b2ba6d17a5524c6b2220f
Natali <n_sunberry@inbox.ru>
2022-09-20 22:38:29 +0300

    solution  array

--- Program.cs
+++ Program.cs
@@ -3,4 +3,14 @@ for (int i = 0; i < array.Length; i++)
 {
     Console.Write($"{array[i]} ");
 }
+Console.WriteLine();
+Console.Write("result: ");
+for (int i = 0; i < array.Length; i++)
+{
+    string array2 = array[i];
+    if (array2.Length <= 3)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
 
