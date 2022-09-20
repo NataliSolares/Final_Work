0894f19144d68db0f5d0eb47c4bf4d70d2d96a0d
Natali <n_sunberry@inbox.ru>
2022-09-20 22:21:42 +0300

    add array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[] { "3", "---", "string", "write", "//", "123" };
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+
